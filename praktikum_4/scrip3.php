6dd7f33d401f22286302a2e6faa67dd8b8c0d2ae
selviblabla <selpi97@gmail.com>
2021-09-27 19:49:03 +0700

    percobaan praktikum

---
+++ praktikum_4/scrip3.php
@@ -0,0 +1,9 @@
+<?php   
+    $anak[0] = "Faruq";
+    $anak[1] = "Alya";
+    $anak[2] = "Zahro";
+    foreach ($anak as $value) {
+        echo "nama anak : $value";
+        echo "<br>";
+    }
+?>
