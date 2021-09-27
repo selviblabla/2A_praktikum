26276a6bbf6cb3e567b35f017000c3aaa1bb8603
selviblabla <selpi97@gmail.com>
2021-09-27 19:25:12 +0700

    coba praktikum_4

---
+++ praktikum_4/scrip2.php
@@ -0,0 +1,11 @@
+<?php
+    $nama[] = "Muhammad";
+    $nama[] =  "faruq"; $nama[] = "Zakiuddin"; echo 
+"Hello";
+    echo "Isi array \$anak[0] adalah $nama[0]"; echo
+"<br>";
+    echo "Isi array \$anak[1] adalah $nama [1]"; echo
+"<br>";
+    echo "Isi array \$anak[2] adalah $nama [1]"; echo
+"<br>";
+?>
