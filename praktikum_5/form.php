8bf875f25793c722cdd2dd82b8e89293a560e9a3
selviblabla <selpi97@gmail.com>
2021-10-04 19:29:31 +0700

    coba praktikum

---
+++ praktikum_5/form.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> form method GET</title>
+</head>
+<body>
+    <h1>Input</h1>
+    <form action="methodgetproses.php"
+method="get">
+        Masukkan nama : <input type="text"
+name="nama" size="30">
+            <input type="submit" value="simpan">
+       </form>
+    </body>
+</html>
